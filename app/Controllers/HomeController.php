1d8c84fa736f5741a6b712f65c8cfae1d4b9d065
Victor Ojeda <ojedavd@hotmail.com>
2018-10-05 10:53:23 -0300

    Using Eloquent

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -7,6 +7,13 @@ use Slim\Views\Twig as View;
 class HomeController extends Controller {
 
     public function index($request, $response) {
+
+        $user = $this->db->table('users')->find(1);
+
+        var_dump($user->email);
+
+        die();
+
         return $this->view->render($response, 'home.twig');
     }
 }
