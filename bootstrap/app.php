6dad15ad0f48d8c54c4e131dcf4775584251a5bd
Victor Ojeda <ojedavd@hotmail.com>
2018-10-09 16:06:25 -0300

    Getting ready to sign up

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -48,4 +48,8 @@ $container['HomeController'] = function($container) {
     return new \App\Controllers\HomeController($container);
 };
 
+$container['AuthController'] = function($container) {
+    return new \App\Controllers\Auth\AuthController($container);
+};
+
 require __DIR__ . '/../app/routes.php';
