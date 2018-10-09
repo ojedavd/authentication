80ddc3c55fadeffc3f0ab2e99eb3e7c2e1d6fb3d
Victor Ojeda <ojedavd@hotmail.com>
2018-10-09 15:24:25 -0300

    The user model

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,17 +2,12 @@
 
 namespace App\Controllers;
 
+use App\Models\User;
 use Slim\Views\Twig as View;
 
 class HomeController extends Controller {
 
     public function index($request, $response) {
-
-        $user = $this->db->table('users')->find(1);
-
-        var_dump($user->email);
-
-        die();
 
         return $this->view->render($response, 'home.twig');
     }
